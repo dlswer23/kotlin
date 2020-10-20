4d5eec234c3c965bc6552edab3fca50511559353
이은별 <lbj4298@naver.com>
2020-10-20 13:19:04 +0900

    [ADD]kotlin기본 입출력

---
+++ Person.kt
@@ -0,0 +1,14 @@
+package com.example.edu
+
+import kotlin.math.PI
+import kotlin.math.abs
+
+fun main(){
+    val intro: String = "안녕하세요!"
+    val num:Int =20;
+
+    println(PI)
+    print(abs(-12.6))
+
+    println(":intro: $intro,num: $num")
+}
